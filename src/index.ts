b358b67b552e9ebd7ec9d1bbb317689aa1ee8dab
TinGoll <roma.oliynyk83@gmail.com>
2022-12-02 13:50:26 +0300

    Add BaseSystem and fix

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,8 @@ import { Engine } from "./core/Engine";
 import { Entity } from "./core/Entity";
 import { EntitySystem } from "./core/EntitySystem";
 import { Family } from "./core/Family";
+import { BaseSystem } from "./systems/BaseSystem";
 import { IteratingSystem } from "./systems/IteratingSystem";
 
-
 export default Engine.create;
-export {
-  Engine,
-  Entity,
-  Component,
-  EntitySystem,
-  Family,
-  IteratingSystem,
-  ComponentClass,
-};
-
-
-
+export { Engine, Entity, Component, EntitySystem, Family, IteratingSystem, ComponentClass, BaseSystem };
